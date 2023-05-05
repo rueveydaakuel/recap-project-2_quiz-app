d1fe9c0d3b0ffa504e8c3c5506210b79e74469b1
Rüveyda Akgül <rakguel94@gmail.com>
2023-05-05 15:03:24 +0200

    toggle functionality bookmark button

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toogle("bookmark--active");
+});
+
+const cardAnswer = document.querySelector('[data-js="card-answer"]');
+const cardAnswerButton = document.querySelector(
+  '[data-js="card-answer-button"]'
+);
+
+cardAnswerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer");
+});
